1d6835b829a2092d100dab9f91444d2787d95872
sergey <frants_s@bk.ru>
2022-10-16 19:19:25 +0300

    задача №2 на нахождение меньшего или большего числа

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+// а какое меньше.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.Write("ввести число №1  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("ввести число №2  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b > a)
+{
+    Console.Write("большее число № 2");
+}
+else
+    Console.Write("меньшее число № 1");
