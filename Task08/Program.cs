61b95ba8b42b4cd26c3b6dd64a9634f24deeed71
sergey <frants_s@bk.ru>
2022-10-17 23:21:54 +0300

    программу, которая на вход принимает число (N),

---
+++ Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+if (N <= 1)
+    Console.WriteLine("такого числа нет");
+
+else
+{
+    while(index < N-1)
+    {
+        index += 2;
+        Console.WriteLine(index);
+    }
+}
+
